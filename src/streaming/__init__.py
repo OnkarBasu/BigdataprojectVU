9d1aa6feb04df3d55158ecaca92136acdb48833f
Anton Bannikov <bannikov.anton.geo@gmail.com>
2026-03-08 02:08:52 +0200

    Added reader into streaming module

--- src/streaming/__init__.py
+++ src/streaming/__init__.py
@@ -1,3 +1,8 @@
 from .parser import AISRowParser
+from .reader import stream_records, stream_csv_in_chunks
 
-__all__ = ["AISRowParser"]
+__all__ = [
+    "AISRowParser",
+    "stream_records",
+    "stream_csv_in_chunks",
+]

7eff97e8cac953083076ac3b009e1d623d335f7c
Anton Bannikov <bannikov.anton.geo@gmail.com>
2026-03-09 17:28:38 +0200

    Updated init for reader module

--- src/streaming/__init__.py
+++ src/streaming/__init__.py
@@ -1,10 +1,17 @@
 from .parser import AISRowParser
-from .reader import stream_records, stream_csv_in_chunks
+from .reader import (
+    stream_records,
+    stream_records_from_files,
+    stream_csv_in_chunks,
+    stream_csv_files_in_chunks,
+)
 from .types import Chunk
 
 __all__ = [
     "AISRowParser",
     "stream_records",
+    "stream_records_from_files",
     "stream_csv_in_chunks",
+    "stream_csv_files_in_chunks",
     "Chunk",
 ]

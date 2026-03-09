18aef440e56290302ba984eec3e796602aa0e63a
Anton Bannikov <bannikov.anton.geo@gmail.com>
2026-03-09 16:01:49 +0200

    Replaced Chunk type into separate types.py

--- src/streaming/__init__.py
+++ src/streaming/__init__.py
@@ -1,8 +1,10 @@
 from .parser import AISRowParser
 from .reader import stream_records, stream_csv_in_chunks
+from .types import Chunk
 
 __all__ = [
     "AISRowParser",
     "stream_records",
     "stream_csv_in_chunks",
+    "Chunk",
 ]

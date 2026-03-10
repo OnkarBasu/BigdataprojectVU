e3b6894fbde89eb7307e27d9e848141fb0d0f2e1
Anton Bannikov <bannikov.anton.geo@gmail.com>
2026-03-10 23:31:44 +0200

    Refactor run_detection.py

--- src/parallel/__init__.py
+++ src/parallel/__init__.py
@@ -2,8 +2,9 @@
 Parallel processing utilities for chunk-based AIS analytics.
 """
 
-from .worker_pool import process_chunk
+from .worker_pool import process_chunk, worker_init
 
 __all__ = [
     "process_chunk",
+    "worker_init",
 ]

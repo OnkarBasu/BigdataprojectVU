2c520c1091b3d83a6b4bf60a670bebbf9c116b13
Anton Bannikov <bannikov.anton.geo@gmail.com>
2026-04-04 20:22:23 +0300

    ref-clean: Deleted redundant attr in VesselGlobalSummary

--- src/models/processing.py
+++ src/models/processing.py
@@ -115,4 +115,3 @@ class VesselGlobalSummary:
     teleportation_d1_events: list[TeleportationEvent] = field(default_factory=list)
     teleportation_d2_events: list[TeleportationEvent] = field(default_factory=list)
     loitering_transfer_events: list[LoiteringTransferEvent] = field(default_factory=list)
-    sampled_records: list[AISRecord] = field(default_factory=list)
